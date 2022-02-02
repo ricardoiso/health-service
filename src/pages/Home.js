081e6c5ea32fc386a216ae02ecb3be3692c4b35d
this_serra <riicardoserrano@gmail.com>
2022-02-02 12:39:20 -0400

    Hero section structure implemented

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
+import Hero from '../components/Hero';
 
 function Home() {
   return (
     <div>
         <Navbar />
+        <Hero />
     </div>
   );
 }
