9761d25154182f7d245653aeadc0ada9cbc91ae7
this_serra <riicardoserrano@gmail.com>
2022-02-02 14:59:48 -0400

    Specialties section added

---
+++ src/components/Doctors.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Doctors() {
+  return (
+    <div>
+        Doctors
+    </div>
+  );
+}
+
+export default Doctors;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
 import Hero from '../components/Hero';
+import Specialties from '../components/Specialties';
+import Doctors from '../components/Doctors';
 
 function Home() {
   return (
     <div>
         <Navbar />
         <Hero />
+        <Specialties />
+        <Doctors />
     </div>
   );
 }
