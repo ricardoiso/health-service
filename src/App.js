5bbb269bd1493a7ef6141eb755fef5aadeafcca2
this_serra <riicardoserrano@gmail.com>
2022-02-02 16:31:01 -0400

    navigation implemented on the site

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from './pages/Home';
+import Login from './pages/Login';
 
-function App() {
+
+/* function App() {
   return (
     <div className="App">
       <Home />
     </div>
   );
-}
+} */
+
+const App = () => (
+  <Router>
+    <Routes>
+      <Route exact path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+    </Routes>
+  </Router>
+);
 
 export default App;
