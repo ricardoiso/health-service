d57b4a773c352b3970706d7fb6d09052ea5b44c7
this_serra <riicardoserrano@gmail.com>
2022-02-03 14:42:27 -0400

    Deatil view done ✅

--- src/components/DetailSection.js
+++ src/components/DetailSection.js
@@ -26,18 +26,22 @@ export default function DetailSection() {
         </div>
         <div className="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
           <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
-            {element && element.name}Algo
+            {element && element.name}
           </h1>
-          <p className="mb-8 leading-relaxed">
-            {element &&
-              element.description.substring(1, element.description.length - 1)}
+          <p className="mb-2 leading-relaxed">
+            
+            {element && (slug === 'medical-items' || slug === 'medical-centers') &&
+            element.direction}
           </p>
-          <h4 className="text-gray-400 ">{element.email}</h4>
-          <h4 className="text-gray-400 mb-3">{element.contact_number}</h4>
-          {/* <div className="flex justify-center">
-          <button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button>
-          <button className="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">Button</button>
-        </div> */}
+          <p className="mb-8 leading-relaxed">
+            {element && (slug ==='doctors' || slug === 'medical-centers') &&
+              element.description.substring(1, element.description.length - 1)}
+            {element && slug === 'medical-items' && 
+            element.description}
+          </p>
+          <h4 className="text-gray-400 ">{ slug === 'doctors' && element.email}</h4>
+          <h4 className="text-gray-400 mb-3">{ slug === 'doctors' && element.contact_number}</h4>
+          
         </div>
       </div>
     </section>
