19cc8cfd6c530e4120c41e22805472edd4332322
this_serra <riicardoserrano@gmail.com>
2022-02-03 11:21:51 -0400

    information is displayed depending on the route

--- src/pages/search.js
+++ src/pages/search.js
@@ -6,15 +6,40 @@ import { useParams } from "react-router-dom";
 import { useSearch } from "../hooks/useSearch.js";
 
 function SearchPage() {
-  const [medicines, setMedicines] = useState([]);
-  const [supplies, setSupplies] = useState([]);
+  const [items, setItems] = useState([]);
+  const [centers, setCenters] = useState([]);
   const [doctors, setDoctors] = useState([]);
-  const [medicalOrg, setMedicalOrg] = useState([]);
 
   const data = useSearch();
+  const category = useParams().category;
+  console.log(data);
 
-  console.log(data)
-  
+
+
+  /* try {
+    if (items.length === 0 && centers.length === 0 && doctors.length === 0) {
+      switch (category) {
+        case 'medical-items':
+          setItems(data.data);
+          console.log('Items done: ', items);
+          break;
+          case 'medical-centers':
+            setCenters(data.data);
+            console.log('Centers done: ', centers);
+            break;
+            case 'doctors':
+              setDoctors(data.data);
+              console.log('Doctors done: ', doctors);
+          break;
+        default: 
+          break;
+      } 
+    }
+  } catch (error) {
+    console.error(error);
+  } */
+
+
 
   return (
     <div className="m-4">
@@ -37,7 +62,51 @@ function SearchPage() {
         <section className="text-gray-600 body-font">
           <div className="container px-5 py-24 mx-auto flex flex-wrap">
             <div className="flex flex-wrap -m-4">
-              <div className="p-4 lg:w-1/2 md:w-full">
+
+              { data.data &&
+                data.data.map((element, index) => (
+                  <div className="p-4 lg:w-1/2 md:w-full" key={index}>
+                <div className="flex border-2 rounded-lg border-gray-200 border-opacity-50 p-8 sm:flex-row flex-col">
+                  <div className="w-16 h-16 sm:mr-8 sm:mb-0 mb-4 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 flex-shrink-0">
+                    <svg
+                      fill="none"
+                      stroke="currentColor"
+                      className="w-8 h-8"
+                      viewBox="0 0 24 24"
+                    >
+                      <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
+                    </svg>
+                  </div>
+                  <div className="flex-grow">
+                    <h2 className="text-gray-900 text-lg title-font font-medium mb-3">
+                      { element.name }
+                    </h2>
+                    <p className="leading-relaxed text-base">
+                      Blue bottle crucifix vinyl post-ironic four dollar toast
+                      vegan taxidermy. Gastropub indxgo juice poutine.
+                    </p>
+                    <a className="mt-3 text-indigo-500 inline-flex items-center">
+                      Learn More
+                      <svg
+                        fill="none"
+                        stroke="currentColor"
+                        className="w-4 h-4 ml-2"
+                        viewBox="0 0 24 24"
+                      >
+                        <path d="M5 12h14M12 5l7 7-7 7"></path>
+                      </svg>
+                    </a>
+                  </div>
+                </div>
+              </div>
+                ))
+              }
+
+
+
+
+              
+              {/* <div className="p-4 lg:w-1/2 md:w-full">
                 <div className="flex border-2 rounded-lg border-gray-200 border-opacity-50 p-8 sm:flex-row flex-col">
                   <div className="w-16 h-16 sm:mr-8 sm:mb-0 mb-4 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 flex-shrink-0">
                     <svg
@@ -105,7 +174,7 @@ function SearchPage() {
                     </a>
                   </div>
                 </div>
-              </div>
+              </div> */}
             </div>
           </div>
         </section>
