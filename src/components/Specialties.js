9379aae742514964659a8042a17218aa7b909eb6
this_serra <riicardoserrano@gmail.com>
2022-02-02 22:18:18 -0400

    Cards for search page and change 'class' for 'className'

--- src/components/Specialties.js
+++ src/components/Specialties.js
@@ -2,91 +2,91 @@ import React from 'react';
 
 function Specialties() {
   return (
-    <section class="text-gray-600 body-font">
-  <div class="container px-5 py-12 mx-auto">
-    <div class="flex flex-col text-center w-full mb-10">
-      <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">Historia Clínica para todas las Especialidades</h1>
-      <p class="lg:w-2/3 mx-auto leading-relaxed text-base">El área de especialidades manejadas dentro de la plataforma.</p>
+    <section className="text-gray-600 body-font">
+  <div className="container px-5 py-12 mx-auto">
+    <div className="flex flex-col text-center w-full mb-10">
+      <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">Historia Clínica para todas las Especialidades</h1>
+      <p className="lg:w-2/3 mx-auto leading-relaxed text-base">El área de especialidades manejadas dentro de la plataforma.</p>
     </div>
-    <div class="flex flex-wrap mx-16">
-      <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
-        <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
-          <img alt="team" class="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="https://dummyimage.com/80x80" />
-          <div class="flex-grow">
-            <h2 class="text-gray-900 title-font font-medium">Traumatología</h2>
-            {/* <p class="text-gray-500">UI Designer</p> */}
+    <div className="flex flex-wrap mx-16">
+      <div className="p-2 lg:w-1/3 md:w-1/2 w-full">
+        <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+          <img alt="team" className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="https://dummyimage.com/80x80" />
+          <div className="flex-grow">
+            <h2 className="text-gray-900 title-font font-medium">Traumatología</h2>
+            {/* <p className="text-gray-500">UI Designer</p> */}
           </div>
         </div>
       </div>
-      <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
-        <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
-          <img alt="team" class="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="https://dummyimage.com/84x84" />
-          <div class="flex-grow">
-            <h2 class="text-gray-900 title-font font-medium">Oncología</h2>
-            {/* <p class="text-gray-500">CTO</p> */}
+      <div className="p-2 lg:w-1/3 md:w-1/2 w-full">
+        <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+          <img alt="team" className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="https://dummyimage.com/84x84" />
+          <div className="flex-grow">
+            <h2 className="text-gray-900 title-font font-medium">Oncología</h2>
+            {/* <p className="text-gray-500">CTO</p> */}
           </div>
         </div>
       </div>
-      <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
-        <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
-          <img alt="team" class="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="https://dummyimage.com/88x88" />
-          <div class="flex-grow">
-            <h2 class="text-gray-900 title-font font-medium">Artritis</h2>
-            {/* <p class="text-gray-500">Founder</p> */}
+      <div className="p-2 lg:w-1/3 md:w-1/2 w-full">
+        <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+          <img alt="team" className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="https://dummyimage.com/88x88" />
+          <div className="flex-grow">
+            <h2 className="text-gray-900 title-font font-medium">Artritis</h2>
+            {/* <p className="text-gray-500">Founder</p> */}
           </div>
         </div>
       </div>
-      <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
-        <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
-          <img alt="team" class="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="https://dummyimage.com/90x90" />
-          <div class="flex-grow">
-            <h2 class="text-gray-900 title-font font-medium">Alzheimer</h2>
-            {/* <p class="text-gray-500">DevOps</p> */}
+      <div className="p-2 lg:w-1/3 md:w-1/2 w-full">
+        <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+          <img alt="team" className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="https://dummyimage.com/90x90" />
+          <div className="flex-grow">
+            <h2 className="text-gray-900 title-font font-medium">Alzheimer</h2>
+            {/* <p className="text-gray-500">DevOps</p> */}
           </div>
         </div>
       </div>
-      <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
-        <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
-          <img alt="team" class="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="https://dummyimage.com/94x94" />
-          <div class="flex-grow">
-            <h2 class="text-gray-900 title-font font-medium">Asma</h2>
-            {/* <p class="text-gray-500">Software Engineer</p> */}
+      <div className="p-2 lg:w-1/3 md:w-1/2 w-full">
+        <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+          <img alt="team" className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="https://dummyimage.com/94x94" />
+          <div className="flex-grow">
+            <h2 className="text-gray-900 title-font font-medium">Asma</h2>
+            {/* <p className="text-gray-500">Software Engineer</p> */}
           </div>
         </div>
       </div>
-      <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
-        <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
-          <img alt="team" class="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="https://dummyimage.com/98x98" />
-          <div class="flex-grow">
-            <h2 class="text-gray-900 title-font font-medium">Diábetes</h2>
-            {/* <p class="text-gray-500">UX Researcher</p> */}
+      <div className="p-2 lg:w-1/3 md:w-1/2 w-full">
+        <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+          <img alt="team" className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="https://dummyimage.com/98x98" />
+          <div className="flex-grow">
+            <h2 className="text-gray-900 title-font font-medium">Diábetes</h2>
+            {/* <p className="text-gray-500">UX Researcher</p> */}
           </div>
         </div>
       </div>
-      <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
-        <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
-          <img alt="team" class="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="https://dummyimage.com/100x90" />
-          <div class="flex-grow">
-            <h2 class="text-gray-900 title-font font-medium">Atticus Finch</h2>
-            <p class="text-gray-500">QA Engineer</p>
+      <div className="p-2 lg:w-1/3 md:w-1/2 w-full">
+        <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+          <img alt="team" className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="https://dummyimage.com/100x90" />
+          <div className="flex-grow">
+            <h2 className="text-gray-900 title-font font-medium">Atticus Finch</h2>
+            <p className="text-gray-500">QA Engineer</p>
           </div>
         </div>
       </div>
-      <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
-        <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
-        <img alt="team" class="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="https://dummyimage.com/104x94" />
-          <div class="flex-grow">
-            <h2 class="text-gray-900 title-font font-medium">Alper Kamu</h2>
-            <p class="text-gray-500">System</p>
+      <div className="p-2 lg:w-1/3 md:w-1/2 w-full">
+        <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+        <img alt="team" className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="https://dummyimage.com/104x94" />
+          <div className="flex-grow">
+            <h2 className="text-gray-900 title-font font-medium">Alper Kamu</h2>
+            <p className="text-gray-500">System</p>
           </div>
         </div>
       </div>
-      <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
-        <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
-          <img alt="team" class="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="https://dummyimage.com/108x98" />
-          <div class="flex-grow">
-            <h2 class="text-gray-900 title-font font-medium">Rodrigo Monchi</h2>
-            <p class="text-gray-500">Product Manager</p>
+      <div className="p-2 lg:w-1/3 md:w-1/2 w-full">
+        <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+          <img alt="team" className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="https://dummyimage.com/108x98" />
+          <div className="flex-grow">
+            <h2 className="text-gray-900 title-font font-medium">Rodrigo Monchi</h2>
+            <p className="text-gray-500">Product Manager</p>
           </div>
         </div>
       </div>
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -13,48 +13,48 @@ function Login() {
     >
       <div className="h-screen w-full">
         <div
-          class="w-1/3 bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 flex flex-col mx-auto"
+          className="w-1/3 bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 flex flex-col mx-auto"
           style={{ marginTop: "150px" }}
         >
 
-          <div class="mb-4">
+          <div className="mb-4">
             <label
-              class="block text-grey-darker text-sm font-bold mb-2"
+              className="block text-grey-darker text-sm font-bold mb-2"
               for="username"
             >
               Usuario
             </label>
             <input
-              class="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker"
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker"
               id="username"
               type="text"
               placeholder="Username"
             />
           </div>
-          <div class="mb-6">
+          <div className="mb-6">
             <label
-              class="block text-grey-darker text-sm font-bold mb-2"
+              className="block text-grey-darker text-sm font-bold mb-2"
               for="password"
             >
               Contraseña
             </label>
             <input
-              class="shadow appearance-none border border-red rounded w-full py-2 px-3 text-grey-darker mb-3"
+              className="shadow appearance-none border border-red rounded w-full py-2 px-3 text-grey-darker mb-3"
               id="password"
               type="password"
               placeholder="******************"
             />
-            <p class="text-red text-xs italic">Por favor ingresa tu contraseña.</p>
+            <p className="text-red text-xs italic">Por favor ingresa tu contraseña.</p>
           </div>
-          <div class="flex items-center justify-between">
+          <div className="flex items-center justify-between">
             <button
-              class="bg-blue hover:bg-blue-dark text-white font-bold py-2 px-4 rounded"
+              className="bg-blue hover:bg-blue-dark text-white font-bold py-2 px-4 rounded"
               type="button"
             >
               Sign In
             </button>
             <a
-              class="inline-block align-baseline font-bold text-sm text-blue hover:text-blue-darker"
+              className="inline-block align-baseline font-bold text-sm text-blue hover:text-blue-darker"
               href="#"
             >
               ¿Olvidaste tu contraseña?
