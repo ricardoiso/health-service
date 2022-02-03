eb3066cdc5e7a8ca0f7db2040dc0c74cb1a690a2
Quiarom <userquiarom@gmail.com>
2022-02-03 14:10:24 -0400

    Fixing Anchor tag Backend and Frontend

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,20 +3,20 @@ function Footer() {
     <div className="flex flex-col">
       <footer className="w-full min-h-50 left-0 bottom-0 text-gray-600 body-font">
         <div className="container p-5 mx-auto flex items-center sm:flex-row flex-col">
-          <p className="text-sm text-gray-500 ml-3 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0">
+          <p className="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0">
             © IQ Curi - 2022
           </p>
-          <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
+          <span className="inline-flex space-x-4 sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
             <a
               href="https://github.com/JhormaMalave/hackaton-caracas-api"
-              className="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4"
+              className="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0"
             >
               Backend
             </a>
 
             <a
               href="https://github.com/ricardoiso/health-service"
-              className="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mr-5"
+              className="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0"
             >
               Frontend
             </a>
